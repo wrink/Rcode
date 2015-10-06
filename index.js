bf9047591aa26de00452da32f4cb9857171a01f6
David Wikler <david@Davids-MacBook-Pro-2.local>
2015-10-05 22:41:13 -0400

    multiple files implemented. need file management page

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ var fs = require('fs');
 
 app.use(express.static(__dirname));
 
-var text;
-var file = 'file.txt';
-
 function newUpdate(html, caret, key)
 {
 	return {
@@ -19,41 +16,40 @@ function newUpdate(html, caret, key)
 	};
 }
 
-fs.readFile(file, function(err, data) {
-	if (err) {
-		text = '';
-		console.log(err);
-	}
-	else text = data.toString('utf8');
+app.get('/(:filename)~(:extension)', function(req, res) {
+	res.sendFile(__dirname + '/editor.html');
 
-	fs.writeFileSync(file, text);
+	var text ='';
+	var file = '../RcodeFiles/' + req.params.filename + '.' + req.params.extension;
 
-	app.get('/', function(req, res) {
-		res.sendFile(__dirname + '/editor.html');
+	fs.readFile(file, function(err, data) {
+		if (err) {
+			text = '';
+			console.log(err);
+		}
+		else text = data.toString('utf8');
+
+		fs.writeFileSync(file, text);
+
+		io.on('connection', function(socket){
+			console.log('a user connected: ', socket.id);
+
+			io.emit('update', newUpdate(text, { begin: 0, end: 0}, 0));
+
+			socket.on('disconnect', function() {
+				console.log('user disconnected');
+			});
+
+			socket.on('update', function(update) {
+				text = update.html;
+				fs.writeFileSync(file, text);
+
+				io.emit ('update', newUpdate(text, update.caret, update.key));
+			});
+		});
 	});
-	app.get('/(:id)', function(req, res) {
-		res.sendFile(__dirname + '/editor.html');
-	});
+});
 
-	io.on('connection', function(socket){
-		console.log('a user connected: ', socket.id);
-
-		io.emit('update', newUpdate(text, { begin: 0, end: 0}, 0));
-
-		socket.on('disconnect', function() {
-  		  console.log('user disconnected');
-  		});
-
-  		socket.on('update', function(update) {
-  			text = update.html;
-  			fs.writeFileSync(file, text);
-
-  			io.emit ('update', newUpdate(text, update.caret, update.key));
-  		});
-	});
-
-
-	http.listen(3000, function() {
-		console.log('listening on *:3000');
-	});
+http.listen(3000, function() {
+	console.log('listening on *:3000');
 });
